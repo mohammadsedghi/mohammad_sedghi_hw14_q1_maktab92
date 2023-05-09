781d5a94e7b46e7c49b2b6fd322d6c93ae66baf0
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-05-09 18:23:02 +0430

    search in all file  and check is empty is written

--- src/project.sh
+++ src/project.sh
@@ -4,8 +4,8 @@ numberMenu=0
 touch completedTask.txt
 touch removedTask.txt
 while [ true ]; do
-  echo "select menu item: 1-add Task             2-show remaining task    3-move completed task from taskList file"
-  echo "                  4-show completedTask   5-remove Task            6-show removedTask  7-exit"
+  echo "select menu item: 1-add Task             2-show remaining task    3-move completed task from taskList file  4-show completedTask"
+  echo "                  5-remove Task          6-show removedTask       7-search                                  8- exit"
   read numberMenu
   case $numberMenu in
   1)
@@ -19,48 +19,62 @@ while [ true ]; do
     fi
     ;;
   2)
-    cat taskList.txt
+   if [[ ! -z $(cat taskList.txt) ]] ; then
+       cat taskList.txt
+   else
+       echo "taskList.txt is empty"
+   fi
+
     ;;
   3)
-   echo "please enter your task"
-   read message
-   if grep -Fxq "$message" taskList.txt
-   then
-       echo $message >> completedTask.txt
-         echo $message >> removedTask.txt
-      sed -i  "/$message/d" taskList.txt
-   else
+    echo "please enter your task"
+    read message
+    if grep -Fxq "$message" taskList.txt; then
+      echo $message >>completedTask.txt
+      echo $message >>removedTask.txt
+      sed -i "/$message/d" taskList.txt
+    else
       echo "not found!!!"
-   fi
+    fi
     ;;
   4)
     cat completedTask.txt
     ;;
   5)
     echo "please enter your task"
-     read message
-     if grep -Fxq "$message" taskList.txt
-     then
-           echo $message >> removedTask.txt
-        sed -i  "/$message/d" taskList.txt
-     else
-        echo "not found!!!"
-     fi
+    read message
+    if grep -Fxq "$message" taskList.txt; then
+      echo $message >>removedTask.txt
+      sed -i "/$message/d" taskList.txt
+    else
+      echo "not found!!!"
+    fi
     ;;
 
   6)
-   cat removedTask.txt
+    cat removedTask.txt
     ;;
   7)
-     echo "please enter your task"
-         read message
-         if grep -Fxq "$message" taskList.txt
-         then
-               echo
+    echo "please enter your task"
+    read message
+    if grep -Fxq "$message" taskList.txt; then
+      echo "this task is find in taskList"
 
-         else
-            echo "not found!!!"
-         fi
+    else
+      echo "not found!!! in taskList"
+    fi
+    if grep -Fxq "$message" completedTask.txt; then
+      echo "this task is find in completedTask"
+
+    else
+      echo "not found!!! in completedTask"
+    fi
+    if grep -Fxq "$message" removedTask.txt; then
+      echo "this task is find in removedTask"
+
+    else
+      echo "not found!!! in removedTask"
+    fi
     ;;
   8)
     exit
